4c58610ea0cd5abf7e0bd6a2767d9e3c2ae12b94
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-06-18 20:48:11 +0900

    redirect ip addr

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
-const webpack = require('webpack')
-const path = require('path')
+const webpack = require("webpack");
+const path = require("path");
 
 module.exports = {
   entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:3001',
-    'webpack/hot/only-dev-server',
-    path.resolve(__dirname, 'client', 'index.jsx')
+    "react-hot-loader/patch",
+    "webpack-dev-server/client?172.31.12.35:3001",
+    "webpack/hot/only-dev-server",
+    path.resolve(__dirname, "client", "index.jsx"),
   ],
   resolve: {
-    modules: [path.resolve(__dirname, 'node_modules')],
-    extensions: ['*', '.js', '.jsx']
+    modules: [path.resolve(__dirname, "node_modules")],
+    extensions: ["*", ".js", ".jsx"],
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
+        use: ["babel-loader"],
+      },
+    ],
   },
   output: {
-    path: path.resolve(__dirname, 'public'),
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "public"),
+    publicPath: "/",
+    filename: "bundle.js",
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"development"'
-      }
-    })
+      "process.env": {
+        NODE_ENV: '"development"',
+      },
+    }),
   ],
   devServer: {
     port: 3001,
-    contentBase: path.resolve(__dirname, 'public'),
+    contentBase: path.resolve(__dirname, "public"),
     hot: true,
-    historyApiFallback: true
+    historyApiFallback: true,
   },
-  devtool: 'source-map'
+  devtool: "source-map",
 };
