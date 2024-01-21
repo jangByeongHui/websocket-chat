033c8f03bab7935dc61c197118caa51d79cecbd7
JangByeongHui <jmkabc4@gmail.com>
2024-01-21 17:59:09 +0900

    [refactor]: adjust image & introduction text

--- config/users.js
+++ config/users.js
@@ -1,25 +1,25 @@
 module.exports = [
   {
     name: "꽃",
-    lastName: "이정민",
+    lastName: "정민",
     statusText: "꽃은 플라워, 나는 놀라워!",
     image: "users/LJM_1.JPG",
   },
   {
     name: "유채꽃",
-    lastName: "장병희",
+    lastName: "병희",
     statusText: "나도 한번 누구 따라해봤어 꽃빋침",
     image: "users/JBH_1.JPG",
   },
   {
-    name: "Vv",
-    lastName: "이정민",
+    name: "쁘이",
+    lastName: "정민",
     statusText: "힘들땐! 거울을 봐 Vv",
     image: "users/LJM_2.JPG",
   },
   {
     name: "샤이",
-    lastName: "장병희",
+    lastName: "병희",
     statusText: "햇빛보다는 사진이 더 부끄럽네요..",
     image: "users/JBH_2.JPG",
   },
